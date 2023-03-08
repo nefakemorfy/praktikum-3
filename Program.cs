4d4cd99e51794697239129dd79fe88aa0e9cd622
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-08 22:56:11 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-e задание
+            //Random random = new Random();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int x = random.Next(-40, 40);
+            //    Console.WriteLine(x);
+            //}
+            //2-e задание
+            //int count = 0;
+            //Random random = new Random();
+            //for (int i = 1; i <= 15; i++)
+            //{
+            //    Double x = -20 + 50 * random.NextDouble();
+            //    Console.WriteLine(x);
+            //    if (x > 0)
+            //    {
+            //        { count++; }
+            //    }
+            //}
+            //3-e задание
+            //Random rnd = new Random();
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    double num = rnd.NextDouble() * 10 - 5; // генерация числа в интервале (-5, 5)
+            //    Console.Write(num + " ");
+            //    if (num < 0)
+            //    {
+            //        Console.WriteLine(" - отрицательное");
+            //    }
+            //    else if (num > 0)
+            //    {
+            //        Console.WriteLine(" - положительное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" - ноль");
+            //    }
+            //}
+            //4-е задание
+            //Random rand = new Random();
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int num = rand.Next(-30,31);
+            //    Console.Write(num + " ");
+            //    if (num < 0)
+            //    {
+            //        Console.Write("отрицательное ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("положительное ");
+            //    }
+            //    if (num % 2 == 0)
+            //    {
+            //        Console.WriteLine("четное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("нечетное");
+            //    }
+            //}
+            //5-e задание
+            //    Random rand = new Random();
+            //    for (int i = 0; i < 10; i++)
+            //{
+            //        int num = rand.Next(-20, 21);
+            //        if (num > 0)
+            //        {
+            //            Console.Write(num + " ");
+            //            if (num % 2 == 0)
+            //            {
+            //                Console.WriteLine("четное");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("нечетное");
+            //            }
+            //        }
+            //    }
+            Console.Read();
+        }
+    }
+}
